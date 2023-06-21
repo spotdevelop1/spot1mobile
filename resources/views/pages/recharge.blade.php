5e68745f4fbd21f98e58d65ffdeb1f2467e06c10
lalo0724 <auxiliar.programacion7@alternativa19delsur.com>
2023-06-21 12:44:37 -0600

    Pendiente en vista recarga

--- resources/views/pages/recharge.blade.php
+++ resources/views/pages/recharge.blade.php
@@ -1,28 +1,36 @@
 @extends('layouts.index')
 @section('content')
-<section class="pricing bg-shape mt-0 circlu">
+<section class="pricing bg-shape mt-0">
     <div class="container-fluid bg-secondary booking mb-1 mt-0 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container">
         {{--  <div class="container pbRecarga">  --}}
             <div class="row gx-5">
-                <div class="pt-5 mb-6 mt-7">
-                    <h1 class="text-white mb-4 colorBlue col-6 text-center">¡Realiza tus Recargas desde tu casa!</h1>
+
+                <div class="col-lg-6 pt-5 ">
+                    <div class="d-none d-sm-block d-sm-none d-md-block mt-5">
+                        <img src="{{asset('images/person.jpg')}}" alt="" class="w-100 mx-auto d-block">
+                    </div>
                 </div>
-                <div class="col-lg-6">
-                    <div class="color-recarga h-70 d-flex flex-column justify-content-center text-center p-4 wow zoomIn mt-8 border-radius" data-wow-delay="0.6s">
-                        <h3 class="text-black mb-4 text-uppercase">Fácil y Rápido</h3>
-                        <p class="mb-4 text-uppercase text-black">Ingresa tus datos y paga en línea, evita las molestas filas y hazlo todo desde la comidad de tu casa</p>
+
+                <div class="col-lg-6 pt-5 "><!--py-5-->
+                    {{--  <div class="py-5 mt-7">  --}}
+                    <div class="pt-5 mb-6 mt-3">
+                        <h1 class="footer-heading h1 color-primario style-text w700 text-center">¡Realiza tus Recargas desde la comodidad de tu casa</h1>
+                    </div>
+                    <div class="h-70 d-flex flex-column justify-content-center text-center p-4 wow zoomIn mt-8 " data-wow-delay="0.6s">
+                        {{--  <h3 class="text-black mb-4 text-uppercase">Fácil y Rápido</h3>  --}}
+                        <p class="mb-4 text-uppercase color-secundario h5 mt-n2">Ingresa tus datos y paga en línea, evita las molestas filas y hazlo todo desde la comidad de tu casa</p>
                         <form action="{{route('recharges')}}" method="POST" id="formPago">
                             <div class="row g-3">
                                 <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="Ingresa el número" min="10" max="10" style="height: 55px;" id="numeroTelefono" name="numeroTelefono">
+                                    <input type="text" class="form-control border-0 color-bajoPrimario" placeholder="Ingresa el número" min="10" max="10" style="height: 55px;" id="numeroTelefono" name="numeroTelefono">
                                 </div>
                                 <!-- <div class="col-12 col-sm-6">
-                                    <input type="email" class="form-control border-0" placeholder="Your Email" style="height: 55px;">
+                                    <input type="email" class="form-control border-0 color-bajoPrimario" placeholder="Your Email" style="height: 55px;">
                                 </div> -->
                                 <input type="hidden" value="" id="tipoServicioInput" name="tipoServicioInput">
                                 <div class="col-12 col-sm-6">
-                                    <select class="form-select border-0" style="height: 55px;" id="tipoServicio">
+                                    <select class="form-select border-0 color-bajoPrimario" style="height: 55px;" id="tipoServicio">
                                         <option selected disabled value="0">Tipo de Servicio</option>
                                         <option value="MOVIL">Móvil</option>
                                         <option value="MIFI">MIFI</option>
@@ -31,7 +39,7 @@
 
                                 <input type="hidden" value="" id="montoRecargaInput" name="montoRecargaInput">
                                 <div class="col-12 col-sm-12">
-                                <select class="form-select border-0" style="height: 55px;" id="montoRecarga">
+                                <select class="form-select border-0 color-bajoPrimario" style="height: 55px;" id="montoRecarga">
                                     <option selected disabled value="0">Monto a Recargar</option>
                                     <option value="Plan $300 (Internet ilimitado*  Llamadas y SMS ilimitados*  HOT SPOT**, 30D)">Plan $300 (Internet ilimitado*  Llamadas y SMS ilimitados*  HOT SPOT**, 30D)</option>
                                     <option value="Plan $200 (Internet ilimitado*  Llamadas y SMS ilimitados*, 30D)">Plan $200 (Internet ilimitado*  Llamadas y SMS ilimitados*, 30D)</option>
@@ -42,16 +50,10 @@
                                     <textarea class="form-control border-0" placeholder="Special Request"></textarea>
                                 </div> -->
                                 <div class="col-12">
-                                    <button class="btn btn-secondary w-100 py-3" type="button" id="formRecargar">Recargar</button>
+                                    <button class="btn btn-primary w-100 py-3" type="button" id="formRecargar">Recargar</button>
                                 </div>
                             </div>
                         </form>
-                    </div>
-                </div>
-                <div class="col-lg-6 pt-5 "><!--py-5-->
-                    {{--  <div class="py-5 mt-7">  --}}
-                    <div class="position-absolute z-index-2 d-none d-sm-block d-sm-none d-md-block top-img left-img">
-                        <img src="{{asset('images/person.png')}}" alt="" class="w-75 w-40 mx-auto d-block">
                     </div>
                 </div>
             </div>
@@ -59,24 +61,19 @@
     </div>
 </section>
 
-<section>
-    <div>  {{--  class="container"  --}}
+<section class="pricing bg-shape color-barra-principal mt-4">
+    <div class="container">
       <div class="row">
-        <div class="col color-apart p-3">
-          <h3 class="text-white mb-3 text-center p-3 style-text w400">¿Aún no eres parte de <spa class="style-text w800">SpotUno</spa>?</h3>
-          <h5 class="text-white text-center style-text w400">¿Qué esperas? adquiérelo y disfruta de todos los beneficios</h5>
-          <div class="col text-center p-3">
-            <button type="button" class="btn btn-outline-secondary btn-border style-text w400">¡Solicítalo ya!</button>
+        <div class="col-12">
+          <div class="section-title">
+            <div class="header-text mt-5">
+              <h3 class="text-white style-text w500 display-3 font-duda mb-3">Estamos aquí para ayudarte en cada paso del camino</em></h3>
+            </div>
+            <div class="header-text mb-n3">
+              <button type="button" class="btn btn-light br-radiu-contacto ">Contáctanos</button>
+            </div>
           </div>
-        </div>
-        <div class="col color-paquet p-3">
-          <h3 class="text-white mb-3 text-center p-3 style-text w400">Disfruta de todo <span class="style-text w800">nuestros paquetes</span></h3>
-          <h5 class="text-white text-center style-text w400">Queremos ofrecerte solo lo mejor, seleccina, compra y disfruta</h5>
-          <div class="col text-center p-3">
-            <button type="button" class="btn btn-outline-secondary btn-border style-text w400">Ver paquetes</button>
-          </div>
-        </div>
-      </div>
+        </div>     
     </div>
   </section>
 
@@ -100,7 +97,7 @@
       <h2 class="text-white style-text w900 m-5 text-end text-break">Conoce y contrata el mejor servicio de internet en casa</h2>
     </div>  --}}
 
-    
+
   </section>
 
   <section class="m-informacion">
@@ -134,7 +131,7 @@
 
         var tipoServicio = $('#tipoServicio').val();
         var montoRecarga = $('#montoRecarga').val();
-        
+
         if(numeroSinEspacio == ""){
             return Swal.fire({
                 icon: 'error',
